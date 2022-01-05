55cca288cb8e331afde3012cc3ef7202f910503e
xueke <xueke@dustess.com>
2022-01-05 12:12:36 +0800

    add test

--- grpc/template/template_test.go
+++ grpc/template/template_test.go
@@ -9,8 +9,8 @@ import (
 )
 
 func TestServerTemplate_List(t *testing.T) {
-	//conn, err := grpc.Dial("1.14.108.113:8082", grpc.WithInsecure())
-	conn, err := grpc.Dial("127.0.0.1:8082", grpc.WithInsecure())
+	conn, err := grpc.Dial("1.14.108.113:8082", grpc.WithInsecure())
+	//conn, err := grpc.Dial("127.0.0.1:8082", grpc.WithInsecure())
 
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
@@ -25,5 +25,30 @@ func TestServerTemplate_List(t *testing.T) {
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
-	log.Println("name = ", r.Templates[0].Name)
+
+
+	for _, item := range r.Templates {
+		log.Println("item = ", item)
+	}
+
+}
+
+func TestServerTemplate_AddTemplate(t *testing.T) {
+	conn, err := grpc.Dial("1.14.108.113:8082", grpc.WithInsecure())
+	//conn, err := grpc.Dial("127.0.0.1:8082", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	defer conn.Close()
+
+	c := tRpc.NewTemplateRpcServiceClient(conn)
+
+	ok, e := c.AddTemplate(context.Background(), &tRpc.TemplateRpcAdd{Name: "simple"})
+	if e != nil {
+		log.Fatalf("did not connect: %v", e)
+	}
+
+	println("result = ", ok.Ok)
 }
